c5727478c764a9dae1e0be275d8e516cbacf89b0
H40831 <sumipoo@gmail.com>
2019-12-11 10:17:19 +0000

    written moveRooms()

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-var fs = require('fs');
-var http = require('http');
-var server = http.createServer();
+require('dotenv').config();
+const fs = require('fs');
+const http = require('http');
+const server = http.createServer();
+const db = require('mysql').createConnection({
+    host: 'my-rds.c7rekwisb6xy.ap-northeast-1.rds.amazonaws.com',
+    database: 'real_time_chat',
+    charset: 'utf8mb4',
+    user: process.env.DB_ID,
+    password: process.env.DB_PW,
+});
+db.connect();
 
 server.on('request', function(req, res) {
     res.end();
 });
-var io = require('socket.io').listen(server);
-server.listen(8080);
+const io = require('socket.io').listen(server);
+
+const moveRooms = function(socket,roomId){
+    const rooms = ()=> socket.rooms;
+    for( let room in rooms() ){
+        if( room === roomId ){
+            continue;
+        }
+        socket.leave( room );
+    }
+    socket.join(roomId);
+};
+
+const sendMessage = function(socket,name,message){
+    console.log('test');
+};
 
 io.sockets.on('connection', function(socket) {
-    socket.emit('connect', {message: 'connect success.'}, function (data) {
-        console.log('data: ' + data);
+    socket.on('moveRooms', function(roomId){
+        moveRooms(this,roomId);
+    });
+    socket.on('rooms', function(){
+        socket.emit('notification', socket.rooms);
     });
 });
+
+server.listen(8080);
